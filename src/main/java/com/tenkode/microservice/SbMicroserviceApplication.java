7f13ed8c8161292a83cf6d82b43db2b6fc74fbe2
Tim Taylor <tim@taylorhome.org>
2018-05-28 13:06:19 -0500

    First commit to local repo.

---
+++ src/main/java/com/tenkode/microservice/SbMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.tenkode.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMicroserviceApplication.class, args);
+	}
+}
